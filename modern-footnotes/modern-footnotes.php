ada072af1792b39ffd9709658e40910c8d562384
Sean Williams <sean@Prismtechstudios.com>
2017-04-01 13:32:51 -0700

    Set up activation and deactivation hooks
    To get an idea of install and uninstall counts.

--- modern-footnotes/modern-footnotes.php
+++ modern-footnotes/modern-footnotes.php
@@ -13,3 +13,22 @@ License URI: https://www.gnu.org/licenses/lgpl-3.0.en.html
 //don't let users call this file directly
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+function modern_footnotes_activate() {
+	try {
+		file_get_contents('http://prismtechstudios.com/modern-footnotes/activate.php?' .
+							'd=' . urlencode($_SERVER['SERVER_NAME']));
+	} catch (Exception $ex) {
+		//do nothing
+	}
+}
+
+function modern_footnotes_deactivate() {
+	try {
+		file_get_contents('http://prismtechstudios.com/modern-footnotes/deactivate.php?d=' . urlencode($_SERVER['SERVER_NAME']));
+	} catch (Exception $ex) {
+		//do nothing
+	}
+}
+
+register_activation_hook(__FILE__, 'modern_footnotes_activate');
+register_deactivation_hook(__FILE__, 'modern_footnotes_deactivate');
