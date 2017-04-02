14f24ca1345d74c38ed7b297e5d2a603c024ed08
Sean Williams <sean@Prismtechstudios.com>
2017-04-01 18:09:30 -0700

    Fixed an issue where footnote counter was not correct when multiple posts were on the same page.

--- modern-footnotes/modern-footnotes.php
+++ modern-footnotes/modern-footnotes.php
@@ -23,6 +23,12 @@ function modern_footnotes_func($atts, $content = "") {
 	return $content;
 }
 
+//reset the footnote counter for every new post
+function modern_footnotes_reset_count() {
+	global $modern_footnotes_count;
+	$modern_footnotes_count = 1;
+}
+
 function modern_footnotes_activate() {
 	try {
 		file_get_contents('http://prismtechstudios.com/modern-footnotes/activate.php?' .
@@ -44,6 +50,7 @@ register_activation_hook(__FILE__, 'modern_footnotes_activate');
 register_deactivation_hook(__FILE__, 'modern_footnotes_deactivate');
 add_shortcode('modern_footnote', 'modern_footnotes_func');
 add_shortcode('mfn', 'modern_footnotes_func');
+add_filter('the_post', 'modern_footnotes_reset_count');
 
 wp_enqueue_style('modern_footnotes', plugins_url('/modern-footnotes/styles.css'), array(), '1.0');
 wp_enqueue_script('modern_footnotes', plugins_url('/modern-footnotes/modern-footnotes.js'), array('jquery'), '1.0', TRUE);
