a4db9c2428490defe1aa4d0a63cffc6ffb76d301
Sean Williams <sean@Prismtechstudios.com>
2017-04-01 17:55:17 -0700

    Fixed issue with displaying content on non-single post pages.

--- modern-footnotes/modern-footnotes.php
+++ modern-footnotes/modern-footnotes.php
@@ -13,17 +13,13 @@ License URI: https://www.gnu.org/licenses/lgpl-3.0.en.html
 //don't let users call this file directly
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-function modern_footnotes_content_filter($content){
-	$matches = array();
-	if (preg_match_all('/\(\(((.|\n)*?)\)\)/', $content, $matches)) {
-		for ($i = 0; $i < count($matches[0]); $i++) {
-			$replace_with = '<sup class="modern-footnotes-footnote"><a href="#">' . ($i + 1) . '</a></sup>' .
-				'<span class="modern-footnotes-footnote__note">' . $matches[1][$i] . '</span>';
-			$pos = strpos($content, $matches[0][$i]);
-			$content = substr_replace($content, $replace_with, $pos, strlen($matches[0][$i]));
-		}
-	}
-	
+$modern_footnotes_count = 1;
+
+function modern_footnotes_func($atts, $content = "") {
+	global $modern_footnotes_count;
+	$content = '<sup class="modern-footnotes-footnote"><a href="#">' . $modern_footnotes_count . '</a></sup>' .
+				'<span class="modern-footnotes-footnote__note">' . $content . '</span>';
+	$modern_footnotes_count++;
 	return $content;
 }
 
@@ -46,7 +42,8 @@ function modern_footnotes_deactivate() {
 
 register_activation_hook(__FILE__, 'modern_footnotes_activate');
 register_deactivation_hook(__FILE__, 'modern_footnotes_deactivate');
-add_filter( 'the_content', 'modern_footnotes_content_filter');
+add_shortcode('modern_footnote', 'modern_footnotes_func');
+add_shortcode('mfn', 'modern_footnotes_func');
 
 wp_enqueue_style('modern_footnotes', plugins_url('/modern-footnotes/styles.css'), array(), '1.0');
 wp_enqueue_script('modern_footnotes', plugins_url('/modern-footnotes/modern-footnotes.js'), array('jquery'), '1.0', TRUE);
