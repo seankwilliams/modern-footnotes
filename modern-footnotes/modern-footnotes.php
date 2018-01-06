503d9f41c9277a87f0498a481cbb4717d7046f30
Rob Skidmore <rob@robskidmore.me>
2018-01-06 14:09:20 -0700

    fix enqueue errors
    Enqueue styles and scripts at 'wp_enqueue_scripts' hook

--- modern-footnotes/modern-footnotes.php
+++ modern-footnotes/modern-footnotes.php
@@ -38,8 +38,11 @@ add_shortcode('modern_footnote', 'modern_footnotes_func');
 add_shortcode('mfn', 'modern_footnotes_func');
 add_filter('the_post', 'modern_footnotes_reset_count');
 
-wp_enqueue_style('modern_footnotes', plugin_dir_url(__FILE__) . 'styles.min.css', array(), '1.1.0'); 
-wp_enqueue_script('modern_footnotes', plugin_dir_url(__FILE__) . 'modern-footnotes.min.js', array('jquery'), '1.1.0', TRUE);
+add_action( 'wp_enqueue_scripts', 'modern_footenotes_enqueue_scripts_styles' );
+function modern_footenotes_enqueue_scripts_styles() {
+	wp_enqueue_style('modern_footnotes', plugin_dir_url(__FILE__) . 'styles.min.css', array(), '1.1.0'); 
+	wp_enqueue_script('modern_footnotes', plugin_dir_url(__FILE__) . 'modern-footnotes.min.js', array('jquery'), '1.1.0', TRUE);
+}
 
 //
 //modify the admin
