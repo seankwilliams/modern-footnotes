331a977921269e469e272ef5fa00639e8e1a1ab6
Sean Williams <sean@Prismtechstudios.com>
2018-09-14 20:01:20 -0600

    Added a setting for a custom shortcode

--- modern-footnotes/modern-footnotes.php
+++ modern-footnotes/modern-footnotes.php
@@ -13,13 +13,14 @@ License URI: https://www.gnu.org/licenses/lgpl-3.0.en.html
 //don't let users call this file directly
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+$modern_footnotes_options = get_option('modern_footnotes_settings');
+
 $modern_footnotes_count = 1;
 
 function modern_footnotes_func($atts, $content = "") {
-	global $modern_footnotes_count;
+	global $modern_footnotes_count, $modern_footnotes_options;
 	$additional_classes = '';
-	$settings = get_option('modern_footnotes_settings');
-	if (isset($settings['use_expandable_footnotes_on_desktop_instead_of_tooltips']) && $settings['use_expandable_footnotes_on_desktop_instead_of_tooltips']) {
+	if (isset($modern_footnotes_options['use_expandable_footnotes_on_desktop_instead_of_tooltips']) && $modern_footnotes_options['use_expandable_footnotes_on_desktop_instead_of_tooltips']) {
 		$additional_classes = 'modern-footnotes-footnote--expands-on-desktop';
 	}
 	$content = '<sup class="modern-footnotes-footnote ' . $additional_classes . '"><a href="#">' . $modern_footnotes_count . '</a></sup>' .
@@ -36,6 +37,9 @@ function modern_footnotes_reset_count() {
 
 add_shortcode('modern_footnote', 'modern_footnotes_func');
 add_shortcode('mfn', 'modern_footnotes_func');
+if (isset($modern_footnotes_options['modern_footnotes_custom_shortcode']) && !empty($modern_footnotes_options['modern_footnotes_custom_shortcode'])) {
+	add_shortcode($modern_footnotes_options['modern_footnotes_custom_shortcode'], 'modern_footnotes_func');
+}
 add_filter('the_post', 'modern_footnotes_reset_count');
 
 
@@ -89,17 +93,43 @@ function modern_footnotes_register_settings() { // whitelist options
 		__FILE__,
 		'modern_footnotes_option_group_section'
 	);
+	add_settings_field(
+		'modern_footnotes_custom_shortcode',
+		'Modern Footnotes Custom Shortcode',
+		'modern_footnotes_custom_shortcode_element_callback',
+		__FILE__,
+		'modern_footnotes_option_group_section'
+	);
 }
 
 function modern_footnotes_sanitize_callback($plugin_options) {  
+	global $modern_footnotes_options;
+	if (isset($plugin_options['modern_footnotes_custom_shortcode']) && !empty($plugin_options['modern_footnotes_custom_shortcode'])) {
+		//remove invalid characters from shortcode
+		$plugin_options['modern_footnotes_custom_shortcode'] = preg_replace('/[^a-zA-Z0-9-_]/i', '', $plugin_options['modern_footnotes_custom_shortcode']);
+		if ((!isset($modern_footnotes_options['modern_footnotes_custom_shortcode']) || $modern_footnotes_options['modern_footnotes_custom_shortcode'] != $plugin_options['modern_footnotes_custom_shortcode']) &&
+			  shortcode_exists($plugin_options['modern_footnotes_custom_shortcode'])) {
+			add_settings_error( 'modern_footnotes_custom_shortcode', 'shortcode-in-use', 'The shortcode "' . $plugin_options['modern_footnotes_custom_shortcode'] . '" is already in use, please enter a different one' );
+			$plugin_options['modern_footnotes_custom_shortcode'] = '';
+		}
+	}
 	return $plugin_options;
 }
 
 function modern_footnotes_use_expandable_footnotes_on_desktop_instead_of_tooltips_element_callback() {
-	$options = get_option('modern_footnotes_settings');
+	global $modern_footnotes_options;
 	
-	$html = '<input type="checkbox" id="use_expandable_footnotes_on_desktop_instead_of_tooltips" name="modern_footnotes_settings[use_expandable_footnotes_on_desktop_instead_of_tooltips]" value="1"' . checked( 1, isset($options['use_expandable_footnotes_on_desktop_instead_of_tooltips']) && $options['use_expandable_footnotes_on_desktop_instead_of_tooltips'], FALSE ) . '/>';
+	$html = '<input type="checkbox" id="use_expandable_footnotes_on_desktop_instead_of_tooltips" name="modern_footnotes_settings[use_expandable_footnotes_on_desktop_instead_of_tooltips]" value="1"' . checked( 1, isset($modern_footnotes_options['use_expandable_footnotes_on_desktop_instead_of_tooltips']) && $modern_footnotes_options['use_expandable_footnotes_on_desktop_instead_of_tooltips'], FALSE ) . '/>';
 	$html .= '<label for="use_expandable_footnotes_on_desktop_instead_of_tooltips">Use expandable footnotes on desktop insetad of the default tooltip style</label>';
+
+	echo $html;
+}
+
+function modern_footnotes_custom_shortcode_element_callback() {
+	global $modern_footnotes_options;
+	
+	$html = '<input type="text" id="modern_footnotes_custom_shortcode" name="modern_footnotes_settings[modern_footnotes_custom_shortcode]" value="' . (isset($modern_footnotes_options['modern_footnotes_custom_shortcode']) ? $modern_footnotes_options['modern_footnotes_custom_shortcode'] : '') . '" />';
+	$html .= '<label for="modern_footnotes_custom_shortcode">Custom shortcode if you\'d like to use something other than [mfn] or [modern_footnote]. Enter the shortcode without the brackets.</label>';
 
 	echo $html;
 }
